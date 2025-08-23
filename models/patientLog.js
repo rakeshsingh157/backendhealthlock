b530de6a3eeffd2fe5a36d5f6b2a2b6d99c48baf
Rakesh Singh <kumarpatelrakesh222@gmail.com>
2025-08-23 08:31:11 +0530

    ok

--- models/patientLog.js
+++ models/patientLog.js
@@ -1,12 +1,19 @@
 const mongoose = require("mongoose");
+const moment = require("moment-timezone");
 
 const patientLogSchema = new mongoose.Schema({
   doctorId: { type: String, required: true },
   doctorName: { type: String, required: true },
   patientId: { type: String, required: true },
   fileName: { type: String, required: true },
-  date: { type: Date, default: Date.now },
-  time: { type: String, default: () => new Date().toLocaleTimeString() }
+  date: { 
+    type: String, 
+    default: () => moment().tz("Asia/Kolkata").format("YYYY-MM-DD") 
+  },
+  time: { 
+    type: String, 
+    default: () => moment().tz("Asia/Kolkata").format("HH:mm:ss") 
+  }
 }, { timestamps: true });
 
 module.exports = mongoose.model("PatientLog", patientLogSchema);
